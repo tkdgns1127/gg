83aeecf225ec84f80fa18c3a0faa8d7af05ed2ea
tkdgns1127 <tkdgns1127@naver.com>
2021-08-19 10:38:31 +0900

    gg

--- src/Hello.java
+++ src/Hello.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("hello");
 	System.out.println("hello");
+	System.out.println("hello");
+	System.out.println("hello");
 }
 }
