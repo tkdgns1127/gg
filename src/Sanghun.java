e73c6e4cb6736deb99343abcfd82e9315bcf6777
hyun1207 <hyunsniper1207@naver.com>
2021-08-19 18:35:41 +0900

    Test

--- src/Sanghun.java
+++ src/Sanghun.java
@@ -2,8 +2,8 @@
 public class Sanghun {
 
 	public static void main(String[] args) {
-		System.out.println("테스트_상훈");
-		System.out.println("테스트_상훈");
+		System.out.println("테스트_승현");
+		System.out.println("테스트_승현");
 	}
 
 }
