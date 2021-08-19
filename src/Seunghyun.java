a8dd576b2430562202744dc4a376dcd159ef5959
hyun1207 <hyunsniper1207@naver.com>
2021-08-19 18:37:25 +0900

    Test

--- src/Seunghyun.java
+++ src/Seunghyun.java
@@ -1,7 +1,8 @@
 
-public class Sanghun {
+public class Seunghyun {
 
 	public static void main(String[] args) {
+		System.out.println("Å×½ºÆ®_½ÂÇö");
 		System.out.println("Å×½ºÆ®_½ÂÇö");
 		System.out.println("Å×½ºÆ®_½ÂÇö");
 	}
