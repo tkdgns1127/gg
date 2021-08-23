1920dcfd82a74ad81170cfc377eb3c1b9e45e693
hyunnndo <andy.hyundo@gmail.com>
2021-08-23 22:57:24 +0900

    도현도 테스트 6

--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,6 @@
 
 public class Person {
 
-	String asd = "도현도 테스트";
-	String asd1 = "도현도 테스";
 	String name;
 	int age;
 
--- src/SignupTest.java
+++
@@ -1,8 +0,0 @@
-
-public class SignupTest {
-	public static void main(String[] args) {
-		
-	}
-}
-//수정
-//수정2
