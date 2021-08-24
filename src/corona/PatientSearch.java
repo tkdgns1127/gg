62a2fd947c8bd0c9ab9854611c83c546aa655811
hyunnndo <andy.hyundo@gmail.com>
2021-08-24 16:09:32 +0900

    corona19 조회부분

---
+++ src/corona/PatientSearch.java
@@ -0,0 +1,28 @@
+package corona;
+
+import java.util.Scanner;
+
+public class PatientSearch {
+	Scanner scanner = new Scanner(System.in);
+	
+	PatientSearch() {
+		System.out.println("환자 검색 메뉴에 들어왔습니다.");
+		search();
+		}
+	
+	
+	void search() {
+		String searchname = null;
+		
+		System.out.print("이름을 입력하세요. : ");
+		searchname = scanner.nextLine();
+		
+		System.out.println(searchname + "으로 검색중...");
+		//확진자 리스트 조회
+		
+		//치료자 리스트 조회
+		
+	}
+	
+
+}
